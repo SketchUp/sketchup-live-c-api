77f0690283428e3e5e631c035bb5684660bc2ae9
Thomas Thomassen <thomas@thomthom.net>
2020-01-10 15:58:47 +0100

    Cleanups.

--- cext/debug.rb
+++ cext/debug.rb
@@ -2,17 +2,17 @@ module TT
   module Plugins
     module TraceUp
 
-      BUILD_TYPE = ARGV[0]
+      CEXT_BUILD_TYPE = ARGV[0]
 
-      puts "Debugging TraceUp (#{BUILD_TYPE})"
+      puts "Debugging #{self.name.split('::').last} (#{CEXT_BUILD_TYPE})"
 
       ruby_version = RUBY_VERSION.split('.').take(2).join('.')
-      bin_path = File.join(__dir__, 'build', BUILD_TYPE, ruby_version)
-      cext = File.join(bin_path, 'traceup.so')
+      bin_path = File.join(__dir__, 'build', CEXT_BUILD_TYPE, ruby_version)
+      cext = File.join(bin_path, 'traceup')
 
       require cext
 
-      puts "TraceUp::CEXT_VERSION: #{TT::Plugins::TraceUp::CEXT_VERSION}"
+      puts "#{self}::CEXT_VERSION: #{self::CEXT_VERSION}"
 
     end
   end
