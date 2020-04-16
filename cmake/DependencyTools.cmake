b30230af8996d94343947e0160872e682f5ba31c
Thomas Thomassen <thomas@thomthom.net>
2020-04-16 15:23:46 +0200

    Add comment on how binary dependencies can be detected on macOS.

--- cmake/DependencyTools.cmake
+++ cmake/DependencyTools.cmake
@@ -29,6 +29,20 @@ function(validate_dependencies TARGET_NAME BINARY_FILE)
     )
   else()
     # TODO: Add macOS support.
+    # otool -L <binary>
+    #
+    #   tthomas-us-la:2.5 tthomas2$ otool -L example.bundle
+    # example.bundle:
+    # 	@executable_path/../Frameworks/Ruby.framework/Versions/Current/Ruby (compatibility version 2.5.0, current version 2.5.1)
+    # 	/usr/lib/libc++.1.dylib (compatibility version 1.0.0, current version 400.9.4)
+    # 	/usr/lib/libSystem.B.dylib (compatibility version 1.0.0, current version 1252.250.1)
+    #
+    #   tthomas-us-la:image_lib tthomas2$ otool -L imagelib_tests
+    # imagelib_tests:
+    # 	@rpath/SketchUpAPI.framework/Versions/A/SketchUpAPI (compatibility version 1.0.0, current version 1.0.0)
+    # 	/usr/lib/libc++.1.dylib (compatibility version 1.0.0, current version 400.9.4)
+    #   /usr/lib/libSystem.B.dylib (compatibility version 1.0.0, current version 1252.250.1)
+
     message(WARNING "Unable to validate dependencies: Unsupported compiler.")
   endif()
 endfunction()
