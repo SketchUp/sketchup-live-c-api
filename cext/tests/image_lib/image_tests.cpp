ea88c8b4462949dcdaf0ecf5df5e35af8a002a9c
Thomas Thomassen <thomas@thomthom.net>
2020-06-30 18:34:19 +0200

    Make image_tests resistant to platform differences.

--- cext/tests/image_lib/image_tests.cpp
+++ cext/tests/image_lib/image_tests.cpp
@@ -5,8 +5,34 @@
 #include <SketchUpAPI/model/image_rep.h>
 #include "image.h"
 
+
+bool operator==(const SUColor& lhs, const SUColor& rhs) {
+  return lhs.red == rhs.red &&
+         lhs.green == rhs.green &&
+         lhs.green == rhs.green &&
+         lhs.alpha == rhs.alpha;
+}
+
+// Make GoogleTest print SUColor in a more readable manner.
+void PrintTo(const SUColor& value, ::std::ostream* os) {
+  *os << "SUColor("
+      << static_cast<int>(value.red) << ", "
+      << static_cast<int>(value.green) << ", "
+      << static_cast<int>(value.blue) << ", "
+      << static_cast<int>(value.alpha) << ")";
+}
+
+bool operator==(const SUColorOrder& lhs, const SUColorOrder& rhs) {
+  return lhs.red_index == rhs.red_index &&
+         lhs.green_index == rhs.green_index &&
+         lhs.green_index == rhs.green_index &&
+         lhs.alpha_index == rhs.alpha_index;
+}
+
+
 namespace example {
 
+// Helper struct used to keep track of expected image data.
 struct ImageInfo {
   ImageInfo(size_t width, size_t height, size_t num_channels) :
       width(width), height(height),
@@ -22,8 +48,40 @@ struct ImageInfo {
   size_t size = 0;
 };
 
+// Helper struct that let us work with ImageRepRef data regardless of how the
+// RGBA data is stored per platform.
+struct Pixel24bit {
+  #ifdef _WIN32
+  Pixel24bit(SUByte red, SUByte green, SUByte blue) :
+      b(blue), g(green), r(red) {}
+  SUByte b;
+  SUByte g;
+  SUByte r;
+  #else
+  Pixel24bit(SUByte red, SUByte green, SUByte blue) :
+      r(red), g(green), b(blue) {}
+  SUByte r;
+  SUByte b;
+  SUByte g;
+  #endif
+};
+
 class ImageTest : public ::testing::Test {
 protected:
+  static void SetUpTestSuite() {
+    // SUGetColorOrder isn't available at compile time so Pixel24bit et al must
+    // be defined by making some assumptions.
+    // Ensure the assumption of the RGBA order is correct. If implementation
+    // details should change that should be caught here.
+    #ifdef _WIN32
+    SUColorOrder expected{ 2, 1, 0, 3 };
+    #else
+    SUColorOrder expected{ 0, 1, 2, 3 };
+    #endif
+    SUColorOrder actual = SUGetColorOrder();
+    assert(actual == expected);
+  }
+
   ImageTest() : image_data_(2, 2, 3),
       input_image_(SU_INVALID), output_image_(SU_INVALID) {
 
@@ -32,17 +90,19 @@ protected:
 
     SUImageRepCreate(&input_image_);
     assert(SUIsValid(input_image_));
-    std::vector<SUByte> pixels{
-      255,  65,   0,
-      32,    0,   0,
-        0,  90,  64,
-      255, 255, 255,
+    std::vector<Pixel24bit> pixels{
+      { 255,  65,   0 },
+      { 32,    0,   0 },
+      {   0,  90,  64 },
+      { 255, 255, 255 },
     };
-    assert(pixels.size() == image_data_.size);
+    assert(pixels.size() == image_data_.num_pixels);
+    assert(pixels.size() * sizeof(Pixel24bit) == image_data_.size);
 
+    SUByte* data = reinterpret_cast<SUByte*>(pixels.data());
     [[maybe_unused]] auto result = SUImageRepSetData(input_image_,
         image_data_.width, image_data_.height,
-        image_data_.bits_per_pixel, 0, pixels.data());
+        image_data_.bits_per_pixel, 0, data);
     assert(result == SU_ERROR_NONE);
   }
 
@@ -85,7 +145,23 @@ std::vector<SUByte> GetImageData(SUImageRepRef image) {
   std::vector<SUByte> buffer(data_size, 0);
   SUImageRepGetData(image, data_size, buffer.data());
 
+  size_t width = 0, height = 0;
+  SUImageRepGetPixelDimensions(image, &width, &height);
+
+  std::vector<SUColor> colors(width * height, {0, 0, 0, 0});
+  SUImageRepGetDataAsColors(image, colors.data());
+
   return buffer;
+}
+
+std::vector<SUColor> GetImagePixels(SUImageRepRef image) {
+  size_t width = 0, height = 0;
+  SUImageRepGetPixelDimensions(image, &width, &height);
+
+  std::vector<SUColor> colors(width * height, {0, 0, 0, 0});
+  SUImageRepGetDataAsColors(image, colors.data());
+
+  return colors;
 }
 
 
@@ -101,13 +177,13 @@ TEST_F(ImageTest, GreyScaleCopy_GreyScale_0) {
   ASSERT_TRUE(result);
   AssertExpectedImageInfo(image_data_, output_image_);
 
-  std::vector<SUByte> expected{
-      255,  65,   0, 255,
-       32,   0,   0, 255,
-        0,  90,  64, 255,
-      255, 255, 255, 255,
+  std::vector<SUColor> expected{
+      { 255,  65,   0, 255 },
+      {  32,   0,   0, 255 },
+      {   0,  90,  64, 255 },
+      { 255, 255, 255, 255 },
   };
-  auto buffer = GetImageData(output_image_);
+  auto buffer = GetImagePixels(output_image_);
   ASSERT_EQ(expected, buffer);
 }
 
@@ -116,13 +192,13 @@ TEST_F(ImageTest, GreyScaleCopy_GreyScale_50) {
   ASSERT_TRUE(result);
   AssertExpectedImageInfo(image_data_, output_image_);
 
-  std::vector<SUByte> expected{
-      161,  66,  33, 255,
-       17,   1,   1, 255,
-       35,  80,  67, 255,
-      255, 255, 255, 255,
+  std::vector<SUColor> expected{
+      { 184,  89,  57, 255 },
+      {  20,   4,   4, 255 },
+      {  30,  75,  62, 255 },
+      { 255, 255, 255, 255 },
   };
-  auto buffer = GetImageData(output_image_);
+  auto buffer = GetImagePixels(output_image_);
   ASSERT_EQ(expected, buffer);
 }
 
@@ -131,13 +207,13 @@ TEST_F(ImageTest, GreyScaleCopy_GreyScale_100) {
   ASSERT_TRUE(result);
   AssertExpectedImageInfo(image_data_, output_image_);
 
-  std::vector<SUByte> expected{
-       67,  67,  67, 255,
-        3,   3,   3, 255,
-       71,  71,  71, 255,
-      255, 255, 255, 255,
+  std::vector<SUColor> expected{
+      { 114, 114, 114, 255 },
+      {   9,   9,   9, 255 },
+      {  60,  60,  60, 255 },
+      { 255, 255, 255, 255 },
   };
-  auto buffer = GetImageData(output_image_);
+  auto buffer = GetImagePixels(output_image_);
   ASSERT_EQ(expected, buffer);
 }
 
